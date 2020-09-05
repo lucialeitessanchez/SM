4ad794204db8c4fcf91c3ca1830e7a2136ad407a
Lucia <lucialeitessanchez@gmail.com>
2020-09-05 18:05:31 -0300

    6 commit hice todas las validaciones menos la de la fecha me falta armar para que la pantalla baje, porque no puedo tocar el boton registrar. Puse que salga el error en los campos obligatorios con un setError . La pantalla esta terminada (el XML )

--- app/src/main/java/ar/com/lls/sendmeal/MainActivity.java
+++ app/src/main/java/ar/com/lls/sendmeal/MainActivity.java
@@ -24,21 +24,41 @@ public class MainActivity extends AppCompatActivity {
     private final static Integer[] anio = {2020,2021,2022,2023 };
     private SeekBar seekBarCarga;
     private TextView carga;
-    private EditText email,clave,clave2,ccv,correo;
+    private EditText email,clave,clave2,ccv,tarjeta;
     private RadioButton rCredito,rDebito;
     private Switch sw;
     private Button registrar;
-
+    int p=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //alta variables EditText
+        //alta variables EditText y el boton
         email = (EditText)findViewById(R.id.ETEmail);
         clave = (EditText)findViewById(R.id.ETContraseña);
         clave2 = (EditText)findViewById(R.id.ETrpcontraseña);
         ccv = (EditText)findViewById(R.id.ETccv);
-        correo = (EditText)findViewById(R.id.ETEmail);
+        tarjeta = (EditText)findViewById(R.id.numTarjeta);
+        registrar = (Button)findViewById(R.id.buttonRegistrar);
+        registrar.setEnabled(false);
+        //el correo no este vacio
+        if(email.getText().toString().isEmpty()){
+            email.setError("El correo esta vacio");
+        }
+
+        //la clave no sea vacia
+        if(clave.getText().toString().isEmpty()){
+            clave.setError("La clave es vacia ");
+        }
+
+        //la tarjeta vacia
+        if(tarjeta.getText().toString().isEmpty()){
+            tarjeta.setError("El numero de tarjeta es obligatorio ");
+        }
+        //ccv no vacio
+        if(ccv.getText().toString().isEmpty()){
+            ccv.setError("El codigo es obligatorio ");
+        }
         //radiogroups
         rCredito=(RadioButton)findViewById(R.id.rBCredito);
         rDebito=(RadioButton)findViewById(R.id.rBDebito);
@@ -64,13 +84,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter<Integer>(this, simple_spinner_dropdown_item, mes);
         Spinner spinnerMes = (Spinner) findViewById(R.id.spinnerFechames);
         spinnerMes.setAdapter(adapter);
+
         //spinner anio
         ArrayAdapter adapter2 = new ArrayAdapter<Integer>(this, simple_spinner_dropdown_item, anio);
         Spinner spinnerAnio = (Spinner) findViewById(R.id.spinnerFechaanio);
         spinnerMes.setAdapter(adapter);
         //slider carga (seekbar)
         seekBarCarga = (SeekBar) findViewById(R.id.seekBarCargaInicial);
-        //tv
+
         //Valor inicial
         seekBarCarga.setProgress(0);
         //Valor Final
@@ -89,36 +110,44 @@ public class MainActivity extends AppCompatActivity {
             //hace un llamado  cuando se detiene la perilla
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                //guardo el valor donde para de mover
+                p=seekBar.getProgress();
             }
         });
 
         //Botón Registrar
 
-    }
+        if(Validar()){
+            registrar.setEnabled(true);
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Validar()){
+
+                }
+            }
+        });
+    }}
 
 
     public boolean Validar(){
-        //Valida que el correo no este vacio
-        if(correo.getText().toString().isEmpty()){
-            correo.setError("El correo esta vacio");
-            return false;
-        }
+            //Verificar que si ingresó una tarjeta de crédito la fecha de vencimiento por lo menos sea superior a los próximos 3 meses
+
             //llama a la funcion validarEmail para ver si tiene el arroba y los 3 strings detras
             if(!validarEmail()){
                 Toast.makeText(this,"El correo es invalido",Toast.LENGTH_LONG).show();
                 return false;
             }
-                //valida que la clave no sea vacia
-                if(clave.getText().toString().isEmpty()){
-                    Toast.makeText(this,"La clave es vacia ",Toast.LENGTH_LONG).show();
-                    return false;
-                }
                 //valida que las claves sean iguales
                 if((clave.getText().toString()).equals((clave2.getText().toString()))){
                     Toast.makeText(this,"Las claves no coinciden",Toast.LENGTH_LONG).show();
                     return false;
                 }
+                    //carga inicial mayor a 0
+                    if(p == 0 && p<0 ){
+                    return false;
+                                        }
+
 
 return true;
     }
