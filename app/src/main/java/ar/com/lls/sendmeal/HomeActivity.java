84de9b8de3d3b2664ecc3b7b0e8a7fbe17fd6760
Lucia <lucialeitessanchez@gmail.com>
2020-09-16 18:12:01 -0300

    2 commit lab2 - vista y activacion del menu desplegable y la creacion de la toolbar principal

--- app/src/main/java/ar/com/lls/sendmeal/HomeActivity.java
+++ app/src/main/java/ar/com/lls/sendmeal/HomeActivity.java
@@ -1,14 +1,51 @@
 package ar.com.lls.sendmeal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //inicializacion del drawer y nav para la toolbar
+        setToolBar();
+        drawerLayout =(DrawerLayout)findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.navview);
+
+    }
+
+    //agrega la toolbar y le agrega la imagen que cree en drawable (el menu hamburguesa)
+    private void setToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //programa la accion del menu dezplegable
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
