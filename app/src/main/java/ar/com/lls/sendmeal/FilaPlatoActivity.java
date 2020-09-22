dcbed3129868e4b2be0434fb37abe4236bd1d4da
Lucia <lucialeitessanchez@gmail.com>
2020-09-22 19:27:09 -0300

    5 commit lab2 - creacion fila plato

---
+++ app/src/main/java/ar/com/lls/sendmeal/FilaPlatoActivity.java
@@ -0,0 +1,14 @@
+package ar.com.lls.sendmeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilaPlatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fila_plato);
+    }
+}
